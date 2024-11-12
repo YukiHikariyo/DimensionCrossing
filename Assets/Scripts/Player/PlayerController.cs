45987b81e7c7a2a21233c338512051854769a302
Yuki Hikariyo <3109209420@qq.com>
2024-11-12 20:57:08 +0800

    爬梯子

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     private BasePlayerState _groundState;
     private BasePlayerState _jumpState;
     private BasePlayerState _airState;
+    private BasePlayerState _ladderState;
     
     [Space(16)]
     [Header("基础数值")]
@@ -18,19 +19,28 @@ public class PlayerController : MonoBehaviour
     
     public float moveSpeed;
     public float jumpSpeed;
+    public float ladderSpeed;
+    [Space(16)]
     public float jumpTimes;
+    [Space(16)]
+    public float jumpGravity;
+    public float fallGravity;
     
     [Space(16)]
     [Header("范围检测")]
     [Space(16)]
     
     public LayerMask groundLayer;
+    public LayerMask ladderLayer;
     
-    public Vector2   groundCheckCenter;
-    public Vector2   groundCheckSize;
+    public Vector2 groundCheckCenter;
+    public Vector2 groundCheckSize;
     
-    public Vector2   headCheckCenter;
-    public Vector2   headCheckSize;
+    public Vector2 headCheckCenter;
+    public Vector2 headCheckSize;
+    
+    public Vector2 ladderCheckCenter;
+    public Vector2 ladderCheckSize;
 
     private void Awake()
     {
@@ -40,22 +50,25 @@ public class PlayerController : MonoBehaviour
         _groundState = new PlayerGroundState(this);
         _jumpState = new PlayerJumpState(this);
         _airState = new PlayerAirState(this);
+        _ladderState = new PlayerLadderState(this);
     }
 
     private void OnEnable()
     {
         playerInputAction.Gameplay.Jump.started += Jump;
+        playerInputAction.Gameplay.VerticalMove.started += ClimbLadder;
         playerInputAction.Enable();
         
         ChangeState(EPlayerState.Ground);
     }
-
+    
     private void OnDisable()
     {
         _currentState.OnExit();
         
         playerInputAction.Disable();
         playerInputAction.Gameplay.Jump.started -= Jump;
+        playerInputAction.Gameplay.VerticalMove.started -= ClimbLadder;
     }
 
     private void FixedUpdate()
@@ -75,6 +88,9 @@ public class PlayerController : MonoBehaviour
         
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireCube((Vector2)transform.position + headCheckCenter, headCheckSize);
+        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube((Vector2)transform.position + ladderCheckCenter, ladderCheckSize);
     }
 
     public void ChangeState(EPlayerState newState)
@@ -85,6 +101,7 @@ public class PlayerController : MonoBehaviour
                             EPlayerState.Ground => _groundState,
                             EPlayerState.Jump   => _jumpState,
                             EPlayerState.Air    => _airState,
+                            EPlayerState.Ladder => _ladderState,
 
                             _ => null
                         };
@@ -94,14 +111,22 @@ public class PlayerController : MonoBehaviour
     public bool GroundCheck() => Physics2D.OverlapBox((Vector2)transform.position + groundCheckCenter
                                                     , groundCheckSize, 0, groundLayer);
     
-    public bool HeadCheck() => Physics2D.OverlapBox((Vector2)transform.position + headCheckCenter
+    public bool HeadCheck() =>  Physics2D.OverlapBox((Vector2)transform.position + headCheckCenter
                                                     , headCheckSize, 0, groundLayer);
+    
+    public bool LadderCheck() => Physics2D.OverlapBox((Vector2)transform.position + ladderCheckCenter
+                                                    , ladderCheckSize, 0, ladderLayer);
 
     public void HorizontalMove()
     {
         if (playerInputAction.Gameplay.HorizontalMove.IsPressed())
-            rb.linearVelocity = new Vector2(playerInputAction.Gameplay.HorizontalMove.ReadValue<float>() * moveSpeed
-                                          , rb.linearVelocityY);
+            rb.linearVelocityX = playerInputAction.Gameplay.HorizontalMove.ReadValue<float>() * moveSpeed;
+    }
+    
+    public void LadderMove()
+    {
+        rb.linearVelocity = new Vector2(playerInputAction.Gameplay.HorizontalMove.ReadValue<float>() * ladderSpeed
+                                      , playerInputAction.Gameplay.VerticalMove.ReadValue<float>() * ladderSpeed);
     }
 
     public void Jump(InputAction.CallbackContext obj)
@@ -111,5 +136,11 @@ public class PlayerController : MonoBehaviour
 
         jumpTimes--;
         ChangeState(EPlayerState.Jump);
+    }
+    
+    private void ClimbLadder(InputAction.CallbackContext obj)
+    {
+        if (LadderCheck())
+            ChangeState(EPlayerState.Ladder);
     }
 }
--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -23,6 +23,7 @@ public enum EPlayerState
     Ground,
     Jump,
     Air,
+    Ladder,
 }
 
 public class PlayerGroundState : BasePlayerState
@@ -32,6 +33,7 @@ public class PlayerGroundState : BasePlayerState
     public override void OnEnter()
     {
         playerController.jumpTimes = 1;
+        playerController.rb.gravityScale = playerController.fallGravity;
     }
 
     public override void PhysicsUpdate()
@@ -59,7 +61,7 @@ public class PlayerJumpState : BasePlayerState
 
     public override void OnEnter()
     {
-        playerController.rb.gravityScale = 2;
+        playerController.rb.gravityScale = playerController.jumpGravity;
         _upTime = 0.25f;
     }
 
@@ -74,14 +76,19 @@ public class PlayerJumpState : BasePlayerState
     public override void LogicUpdate()
     {
         _upTime -= Time.deltaTime;
-        if (_upTime <= 0 || !playerController.playerInputAction.Gameplay.Jump.IsPressed()
-                         || playerController.HeadCheck())
+        if (!playerController.playerInputAction.Gameplay.Jump.IsPressed())
+        {
+            playerController.rb.linearVelocityY = playerController.jumpSpeed * 0.2f;
             changeState(EPlayerState.Air);
+        }
+        else if (_upTime <= 0 || playerController.HeadCheck())
+            changeState(EPlayerState.Air);
+        
     }
 
     public override void OnExit()
     {
-        playerController.rb.gravityScale = 15;
+        playerController.rb.gravityScale = playerController.fallGravity;
     }
 }
 
@@ -104,11 +111,40 @@ public class PlayerAirState : BasePlayerState
         if (playerController.GroundCheck())
             changeState(EPlayerState.Ground);
 
-        playerController.rb.gravityScale = playerController.rb.linearVelocityY > 0 ? 2 : 15;
+        playerController.rb.gravityScale = playerController.rb.linearVelocityY > 0
+                                               ? playerController.jumpGravity 
+                                               : playerController.fallGravity;
     }
 
     public override void OnExit()
     {
-        playerController.rb.gravityScale = 10;
+        playerController.rb.gravityScale = playerController.fallGravity;
+    }
+}
+
+public class PlayerLadderState : BasePlayerState
+{
+    public PlayerLadderState(PlayerController playerController) : base(playerController) { }
+
+    public override void OnEnter()
+    {
+        playerController.rb.gravityScale = 0;
+        playerController.jumpTimes = 1;
+    }
+
+    public override void PhysicsUpdate()
+    {
+        playerController.LadderMove();
+    }
+
+    public override void LogicUpdate()
+    {
+        if (!playerController.LadderCheck())
+            changeState(EPlayerState.Ground);
+    }
+
+    public override void OnExit()
+    {
+        playerController.rb.gravityScale = playerController.fallGravity;
     }
 }
